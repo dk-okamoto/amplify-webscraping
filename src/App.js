9b24c917e64ec19fec195ca9c5082d431fbfe0cc
Naoki Okamoto <nao430@dkkaraoke.co.jp>
2023-03-30 10:11:51 +0900

    ログインと初期グラフ追加

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import { Amplify } from "aws-amplify";
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
+import LineExample from './Line';
+import { Chart, registerables } from "chart.js";
+Chart.register(...registerables);
 
 import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
@@ -17,6 +20,7 @@ function App({ signOut, user }) {
 	  {user ? (
 		  <>
 		  <h3>私は権限を持っています:{user.username}</h3>
+		  <LineExample />
 		  <button onClick={signOut}>サインアウト</button>
 		  </>
 	  ) : (
